32d1bd3ae587efef81e0ed82bd27f330baf1e5c0
Mohamed <mo.hassan2792@gmail.com>
2023-03-19 01:17:43 +0200

    RBNode

--- src/RBNode.java
+++ src/RBNode.java
@@ -1,5 +1,13 @@
+import java.awt.*;
+
+import static java.awt.Color.RED;
+import static java.awt.Color.BLACK;
+
 public class RBNode<K> extends Node<K> {
+    public Color color;
+    public RBNode parent;
     RBNode(K value) {
         super(value);
+        this.color = RED;
     }
 }
