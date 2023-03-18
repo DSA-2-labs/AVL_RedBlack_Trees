68cb2468128179fa52841d80518fe732e2e5cd48
Elbadry2025 <96208963+Elbadry2025@users.noreply.github.com>
2023-03-19 01:49:16 +0200

    added: rotateleft and rotateright methods

--- src/RedBlackTree.java
+++ src/RedBlackTree.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 public class RedBlackTree<K> extends AbstractBST<K> {
 
     @Override
@@ -8,5 +10,21 @@ public class RedBlackTree<K> extends AbstractBST<K> {
     @Override
     public boolean delete(K key) {
         return false;
+    }
+
+    private void rotateleft(RBNode node){
+        RBNode x = (RBNode) node.right;
+        node.right = x.left;
+        x.left = node ;
+        x.color = node.color;
+        node.color = Color.RED;
+    }
+    private  void rotateright(RBNode node){
+        RBNode x = (RBNode) node.left;
+        node.left = x.right;
+        x.right = node;
+        x.color = node.color;
+        node.color = Color.RED;
+
     }
 }
