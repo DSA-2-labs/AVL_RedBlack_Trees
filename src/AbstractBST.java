1f78b46d6f90216d06f5640418b6b2f023437ee4
Mahmoud Ghlab <96776435+Mahmoudjobdis@users.noreply.github.com>
2023-03-19 02:21:00 +0200

    Update AbstractBST.java

--- src/AbstractBST.java
+++ src/AbstractBST.java
@@ -15,9 +15,9 @@ public abstract class AbstractBST<K> implements BinarySearchTree<K> {
         Node temp=root;
         while(temp!=null)
         {
-            if(temp.value==key)
-                found=true;
-            else if(temp.value>key)
+            if(temp.compareTo(key)==0)
+                {found=true; break;}
+            else if(temp.compareTo(key)>0)
                 temp=temp.left;
             else
                 temp=temp.right;
