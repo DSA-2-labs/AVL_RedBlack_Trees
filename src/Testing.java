3fd2d83f017193d004f089a0e23ea1c0c8975844
Mahmoud Ghlab <96776435+Mahmoudjobdis@users.noreply.github.com>
2023-03-20 17:00:07 +0200

    Create Testing.java

---
+++ src/Testing.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Testing {
+    //AVL tree tests
+    public void test_insertAVL()
+    {}
+    public void test_deleteAVL()
+    {}
+    public void test_SearchAVL()
+    {}
+
+    //Red Black tree test
+    public void test_insertRB()
+    {}
+    public void test_deleteRB()
+    {}
+    public void test_SearchRB()
+    {}
+}
